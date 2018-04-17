06f0d6501e8a47d16421a5e67da63de464504ea0
Matheus Compri <matheus.compri@gmail.com>
2018-04-17 00:21:00 -0300

    Using the token from ENV variable

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+// Getting secret token from ENV
+const token = process.env.DISCORD_TOKEN;
+
+// Checking if token is set
+if(typeof token === 'undefined') {
+  // Use the following command to export your token
+  // export DISCORD_TOKEN=secret-token-here
+  console.error('Set DISCORD_TOKEN env variable with your Discord API token');
+  process.exit(1);
+}
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
@@ -11,4 +22,4 @@ client.on("message", (message) => {
   }
 });
 
-client.login("");
+client.login(token);
